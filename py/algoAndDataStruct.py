f76120b1736f01b1a2388f5a345068d3b4e6a296
Derek Podimatis <89597550+derekpodi@users.noreply.github.com>
2022-04-13 12:20:42 -0700

    Update algoAndDataStruct.py
    Add R3: Sorted Array Set, Selection Sort, Insertion Sort, Merge Sort

--- py/algoAndDataStruct.py
+++ py/algoAndDataStruct.py
@@ -270,4 +270,129 @@ def Set_from_Seq(seq):
                 yield x
                 x = self.find_next(x.key)
     return set_from_seq
-        
+
+
+
+#R3
+class Sorted_Array_Set:
+    def __init__(self):     self.A = Array_Seq()        #O(1)
+    def __len__(self):      return len(self.A)          #O(1)
+    def __iter__(self):     yield from self.A           #O(n)
+    def iter_order(self):   yield from self             #O(n)
+
+    def build(self, X):                                 #O(?)
+        self.A.build(X)
+        self._sort()
+
+    def _sort(self):                                    #O(?)
+        pass
+
+    def _binary_search(self, k, i, j):                  #O(log n)
+        if i >= j:  return i 
+        m = (i+j) // 2
+        x = self.A.get_at(m)
+        if x.key > k:       return self._binary_search(k, i, m - 1)
+        if x.key < k:       return self._binary_search(k, m + 1, j)
+        return m
+
+    def find_min(self):                                 #O(1)
+        if len(self) > 0:   return self.A.get_at(0)
+        else:               return None
+    
+    def find_max(self):                                 #O(1)
+        if len(self) > 0:   return self.A.get_at(len(self) - 1)
+        else:               return None
+    
+    def find(self, k):                                  #O(log n)
+        if len(self) == 0:  return None
+        i = self._binary_search(k, 0, len(self) - 1)
+        x = self.A.get_at(i)
+        if x.key == k:      return x
+        else:               return None
+
+    def find_next(self, k):                             #O(log n)
+        if len(self) == 0:  return None
+        i = self._binary_search(k, 0, len(self) - 1)
+        x = self.A.get_at(i)
+        if x.key > k:       return x
+        if i + 1 < len(self): return self.A.get_at(i + 1)
+        else:               return None
+
+    def find_prev(self, k):                             #O(log n)
+        if len(self) == 0:  return None
+        i = self._binary_search(k, 0, len(self) - 1)
+        x = self.A.get_at(i)
+        if x.key < k:       return x
+        if i>0:             return self.A.get_at(i - 1)
+        else:               return None
+
+    def insert(self, x):                                #O(n)
+        if len(self.A) == 0:
+            self.A.insert_first(x)
+        else:
+            i = self._binary_search(x.key, 0, len(self.A) - 1)
+            k = self.A.get_at(i).key
+            if k == x.key:
+                self.A.set_at(i, x)
+                return False
+            if k > x.key:   self.A.insert_at(i, x)
+            else:           self.A.insert_at(i + 1, x)
+        return True
+
+    def delete(self, k):                                #O(n)
+        i = self._binary_search(k, 0, len(self.A) - 1)
+        assert self.A.get_at(i).key == k
+        return self.A.delete_at(i)
+
+
+#Selection Sort
+#maintains and grows a subset the largest i items in sorted order
+def selection_sort(A):                      # Selection sort array A
+    for i in range(len(A) - 1, 0, -1):      # O(n) loop over array
+        m = i                               # O(1) initial index of max
+        for j in range(i):                  # O(i) search for max in A[:i]
+            if A[m] < A[j]:                 # O(1) check for larger value
+                m = j                       # O(1) new max found
+        A[m], A[i] = A[i], A[m]             # O(1) swap
+
+
+#Insertion Sort
+#maintains and grows a subset of the first i input items in sorted order
+def insertion_sort(A):                      #Insertion sort array A
+    for i in range(1, len(A)):              #O(n) loop over array
+        j = i                               #O(1) initialize pointer
+        while j > 0 and A[j] < A[j-1]:      #O(i) loop over prefix
+            A[j-1], A[j] = A[j], A[j-1]     #O(1) swap
+            j = j - 1                       #O(1) decrement j
+
+
+#Both - In-place - means can be implemented using at most a constant amount of additional space
+#Insertion = stable - means items having the same value will appear in the sort in the same order as they appeared in the input array
+
+
+#Merge Sort - Theta(n log n)
+#recursively sorts the left and right half of the array, and then merges the two halves in linear time
+def merge_sort(A, a = 0, b = None):         # Sort sub-array A[a:b]
+    if b is None:                           # O(1) initialize
+        b = len(A)                          # O(1)
+    if 1 < b - a:                           # O(1) size k = b - a
+        c = (a + b + 1) // 2                # O(1) compute center
+        merge_sort(A, a, c)                 # T(k/2) recursively sort left
+        merge_sort(A, c, b)                 # T(k/2) recursively sort right
+        L, R = A[a:c], A[c:b]               # O(k) copy
+        i, j = 0, 0                         # O(1) initialize pointers
+        while a < b:                        # O(n)
+            if (j >= len(R)) or (i < len(L) and L[i] < R[j]):   # O(1) check side
+                A[a] = L[i]                 # O(1) merge from left
+                i=i+1                       # O(1) decrement left pointer
+            else:
+                A[a] = R[j]                 # O(1) merge from right
+                j = j + 1                   # O(1) decrement right pointer
+            a= a+1                          # O(1) decrement merge pointer
+
+#merge sort not in place
+#https://codepen.io/mit6006/pen/RYJdOG      #https://codepen.io/mit6006/pen/wEXOOq
+
+#Sacrifice some time in building the data structure to speed up order queries - technique called preprocessing
+
+
