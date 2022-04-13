3dbc5611dca323a80eb5fd8c8478f86b72021372
Derek Podimatis <89597550+derekpodi@users.noreply.github.com>
2022-04-13 14:54:21 -0700

    Update algoAndDataStruct.py
    add direct access array, hash table implementations

--- py/algoAndDataStruct.py
+++ py/algoAndDataStruct.py
@@ -396,3 +396,128 @@ def merge_sort(A, a = 0, b = None):         # Sort sub-array A[a:b]
 #Sacrifice some time in building the data structure to speed up order queries - technique called preprocessing
 
 
+#R4
+#Comparison model - worst case = height of algorithm's decision tree (log n)
+#Find(k) operation is most used in interfaces; sorted arrays and balanced BinarySearchTrees are able to support find(k) asymptotically optimally
+
+#Direct Access Array 
+# shallow branch as large as space in computer - worst case constant time search at cost of storage space, u time for order operations
+#use hashing to overcome space obstacle
+class DirectAccessArray:
+    def __init__(self, u):  self.A = [None] * u         # O(u)
+    def find(self, k):      return self.A[k]            # O(1)
+    def insert(self, x):    self.A[x.key] = x           # O(1)
+    def delete(self, k):    self.A[k] = None            # O(1)
+    def find_next(self, k):
+        for i in range(k, len(self.A)):                 # O(u)
+            if A[i] is not None:
+                return A[i]
+    def find_max(self):
+        for i in range(len(self.A) - 1, -1, -1):        # O(u)
+            if A[i] is not None:
+                return A[i]
+    def delete_max(self):
+        for i in range(len(self.A) - 1, -1, -1):        # O(u)
+            x = A[i]
+            if x is not None:
+                A[i] = None
+                return x
+
+#Hash collisions if more points than space, pigeonhole principle. 
+#Store collisions somewhere else in same direct accesss array or elsewhere. Open addressing(in practice) or chaining
+#Chain as collision resolution strategy - allow find, insert, delete
+class Hash_Table_Set:
+    def __init__(self, r = 200):                        # O(1)
+        self.chain_set = Set_from_Seq(Linked_List_Seq)
+        self.A = []
+        self.size = 0
+        self.r = r                                      # 100/self.r = fill ratio
+        self.p = 2**31 - 1
+        self.a = randint(1, self.p - 1)
+        self._compute_bounds()
+        self._resize(0)
+    
+    def __len__(self):  return self.size                # O(1)
+    def __iter__(self):                                 # O(n)
+        for X in self.A:
+            yield from X
+        
+    def build(self, X):                                 # O(n)e
+        for x in X: self.insert(x)
+    
+    def _hash(self, k, m):                              # O(1)
+        return ((self.a * k) % self.p) % m
+
+    def _compute_bounds(self):                          # O(1)
+        self.upper = len(self.A)
+        self.lower = len(self.A) * 100*100 // (self.r*self.r)
+    
+    def _resize(self, n):                               # O(n)
+        if (self.lower >= n) or (n >= self.upper):
+            f = self.r // 100
+            if self.r % 100:     f += 1
+            #f = ceil(r / 100)
+            m= max(n, 1) * f
+            A= [self.chain_set() for _ in range(m)]
+            for x in self:
+                h = self._hash(x.key, m)
+                A[h].insert(x)
+            self.A = A
+            self._compute_bounds()
+
+    def find(self, k):                                  # O(1)e
+        h = self._hash(k, len(self.A))
+        return self.A[h].find(k)
+    
+    def insert(self, x):                                # O(1)ae
+        self._resize(self.size + 1)
+        h = self._hash(x.key, len(self.A))
+        added = self.A[h].insert(x)
+        if added:   self.size += 1
+        return added
+    
+    def delete(self, k):                                # O(1)ae
+        assert len(self) > 0
+        h = self._hash(k, len(self.A))
+        x = self.A[h].delete(k)
+        self.size -= 1
+        self._resize(self.size)
+        return x
+    
+    def find_min(self):                                 # O(n)
+        out = None
+        for x in self:
+            if (out is None) or (x.key < out.key):
+                out = x
+        return out
+
+    def find_max(self):                                 # O(n)
+        out = None
+        for x in self:
+            if (out is None) or (x.key > out.key):
+                out = x
+        return out
+
+    def find_next(self, k):                             # O(n)
+        out = None
+        for x in self:
+            if x.key > k:
+                if (out is None) or (x.key < out.key):
+                    out = x
+        return out
+    
+    def find_prev(self, k):                             # O(n)
+        out = None
+        for x in self:
+            if x.key < k:
+                if (out is None) or (x.key > out.key):
+                    out = x
+        return out
+    
+    def iter_order(self):                               # O(nˆ2)
+        x = self.find_min()
+        while x:
+            yield x
+            x = self.find_next(x.key)
+
+
