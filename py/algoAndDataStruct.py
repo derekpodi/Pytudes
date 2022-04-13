8c10d5c48399ad04410e54ffad7c4d8e0d8f7dfe
Derek Podimatis <89597550+derekpodi@users.noreply.github.com>
2022-04-12 20:57:06 -0700

    Update algoAndDataStruct.py

--- py/algoAndDataStruct.py
+++ py/algoAndDataStruct.py
@@ -1,1 +1,31 @@
- 
+#https://ocw.mit.edu/courses/6-006-introduction-to-algorithms-spring-2020/pages/lecture-notes/
+
+
+class StaticArray:
+    def __init__(self, n):
+        self.data = [None] * n
+    def get_at(self, i):
+        if not (0 <= i < len(self.data)): raise IndexError
+        return self.data[i]
+
+    def set_at(self, i, x):
+        if not (0 <= i < len(self.data)): raise IndexError
+        self.data[i] = x
+
+
+def birthday_match(students):
+    """
+    Find a pair of students with the same birthday
+    Input: tuple of student (name, bday) tuples
+    Output: tuple of student names or None
+    """
+    n = len(students) # O(1)
+    record = StaticArray(n) # O(n)
+    for k in range(n): #n 
+        (name1, bday1) = students[k] # O(1) 
+        for i in range(k): #k
+            (name2, bday2) = record.get_at(i)
+            if bday1 ==bday2:
+                 return (name1, name2)
+        record.set_at(k, (name1, bday1))
+    return None
