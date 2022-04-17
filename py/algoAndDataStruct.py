1bb1b8aaa99f920415e29a55a67bdfe3bfd0688e
Derek Podimatis <89597550+derekpodi@users.noreply.github.com>
2022-04-16 21:52:25 -0700

    Update algoAndDataStruct.py
    Add R13 - dijkstra's shortest path, priority queues

--- py/algoAndDataStruct.py
+++ py/algoAndDataStruct.py
@@ -1681,3 +1681,147 @@ Such a path cannot be simple, so it must contain a negative weight cycle.
 #find vertex that mins this sum == intersection w/ max fun for each house
 
 
+
+#R13
+#Dijkstra's Algorithm
+#Most commononly used shortest path algorithm, asymptotically faster than Bellman-Ford
+#Only applies to non-negative edge weight graphs
+#discretizes this continuous process by repeatedly relaxing edges from a vertex whose minimum 
+# weight path estimate is smallest among vertices whose out-going edges have not yet been relaxed
+
+#Often presented in terms of a minimum priority queue data structure
+#run time depends on efficiency of priority queue operations
+
+def dijkstra(Adj, w, s):
+    d = [float('inf') for _ in Adj]         # shortest path estimates d(s, v)
+    parent = [None for _ in Adj]            # initialize parent pointers
+    d[s], parent[s] = 0, s                  # initialize source
+    Q = PriorityQueue()                     # initialize empty priority queue
+    V = len(Adj)                            # number of vertices
+    for v in range(V):                      # loop through vertices
+        Q.insert(v, d[v])                   # insert vertex-estimate pair
+    for _ in range(V):                      # main loop
+        u = Q.extract_min()                 # extract vertex with min estimate
+        for v in Adj[u]:                    # loop through out-going edges
+            try_to_relax(Adj, w, d, parent, u, v)
+            Q.decrease_key(v, d[v])         # update key of vertex
+    return d, parent
+
+# Init shortest path weight estimate and parent pointers. Init priority queue with all vertices from graph
+# Main loop that extracts vertex with min estimate. Relax out-going edges from u
+# Relaxation may reduce the shortest path weight estimate d(s, v), vertex v’s key in the queue must be updated (if it still exists in the queue)
+
+#key observation is that shortest path weight estimate of vertex u equals its actual shortest path
+#  weight d(s, u) = δ(s, u) when u is removed from the priority queue
+#Via upper bound property,d(s, u) = δ(s, u) will still hold at termination of the algorithm
+
+
+#Priority Queues
+#Priority queue maintains a set of key-value pairs, where vertex v is a value and d(s, v) is its key
+#Empty Init, supports 3 operations: insert(val, key), extract_min(), decrease_key(val, new_key)
+#Runtime of dijksra depends on runtime of these operations:
+    # TDijkstra =O(|V|·Ti +|V|·Te +|E|·Td)
+
+#Simplest implementation is to store all the vertices and their current shortest path estimate in a dictionary
+#Hash table of size O(|V|) can support expected constant time O(1) insertion and decrease-key operations,
+#  though to find and extract the vertex with minimum key takes linear time O(|V|)
+#If vertices and indices into the vertex set with a linear range, then can alternatively use a
+# direct access array. Leads to worst case O(1) time insertion and decrease key, remains 
+# linear O|V|) to find and extract the vertex with minimum key
+
+#Runtime for Dijkstra simplifies to:   TDict = O(|V |2 + |E|)
+
+#for dense graphs, |E| = Ω(|V |2),  implementation is linear in the size of input
+
+class PriorityQueue:                        # Hash Table Implementation
+    def __init__(self):                     # stores keys with unique labels
+        self.A = ()
+
+    def insert(self, label, key):           # insert labeled key
+        self.A[label] = key
+    
+    def extract_min(self):                  # return a label with minimum key
+        min_label = None
+        for label in self.A:
+            if (min_label is None) or (self.A[label] < self.A[min_label].key):
+                min_label = label
+        del self.A[min_label]
+        return min_label
+
+    def decrease_key(self, label, key):     # decrease key of a given label
+        if (label in self.A) and (key < self.A[label]):
+            self.A[label] = key
+
+#If the graph is sparse, |E| = O(|V |), we can speed things up with more sophisticated priority queue implementations.
+# vertex can maintain a pointer to its stored location within the heap, or the heap can maintain 
+# a mapping from values (vertices) to locations within the heap
+#solution can support finding a given value in the heap in constant time
+#after decreasing the value’s key, one can restore the min heap property in logarithmic time by re-heapifying the tree
+#binary heap can support each of the three operations in O(log |V |) time
+# the running time of Dijkstra will be:  THeap =O((|V|+|E|)log|V|)
+#For sparse graphs, that’s O(|V|log |V|)
+#Graphs in-between sparse and dense, Fibonacci Heap -- TFibHeap =O(|V|log|V|+|E|) run time
+
+class Item:
+    def __init__(self, label, key):
+        self.label, self.key = label, key
+
+class PriorityQueue:                        # Binary Heap Implementation
+    def __init__(self):                     # stores keys with unique labels
+        self.A = []
+        self.label2idx = {}
+    
+    def min_heapify_up(self, c):
+        if c == 0: return 
+        p = (c-1) // 2
+        if self.A[p].key > self.A[c].key:
+            self.A[c], self.A[p] = self.A[p], self.A[c]
+            self.label2idx[self.A[c].label] = c
+            self.label2idx[self.A[p].label] = p
+            self.min_heapify_up(p)
+    
+    def min_heapify_down(self, p): 
+        if p >= len(self.A): return
+        l = 2 * p + 1
+        r = 2 * p + 2
+        if l >= len(self.A): l = p
+        if r >= len(self.A): r = p
+        c = l if self.A[r].key > self.A[l].key else r
+        if self.A[p].key > self.A[c].key:
+            self.A[c], self.A[p] = self.A[p], self.A[c]
+            self.label2idx[self.A[c].label] = c
+            self.label2idx[self.A[p].label] = p
+            self.min_heapify_down(c)
+        
+    def insert(self, label, key):           # insert labeled key
+        self.A.append(Item(label, key))
+        idx = len(self.A) - 1
+        self.label2idx[self.A[idx].label] = idx
+        self.min_heapify_up(idx)
+    
+    def extract_min(self):                  #remove a label with minimum key
+        self.A[0], self.A[-1] = self.A[-1], self.A[0]
+        self.label2idx[self.A[0].label] = 0
+        del self.label2idx[self.A[-1].label]
+        min_label = self.A.pop().label
+        self.min_heapify_down(0)
+        return min_label
+
+    def decrease_key(self, label, key):     # decrease key of a given label
+        if label in self.label2idx:
+            idx = self.label2idx[label]
+            if key < self.A[idx].key:
+                self.A[idx].key = key
+                self.min_heapify_up(idx)
+
+#Fibonacci Heaps are not actually used very often in practice as it is more complex to implement,
+#  and results in larger constant factor overhead than the other two implementations 
+
+#When the number of edges in the graph is known to be at most linear (e.g., planar or bounded degree graphs)
+#  or at least quadratic (e.g. complete graphs) in the number of vertices, then using a binary heap or dictionary 
+# respectively will perform as well asymptotically as a Fibonacci Heap.
+
+#https://codepen.io/mit6006/pen/BqgXWM
+
+
+
